0da1deabbaa2133d4d7ae5f872653ab91b1a55d5
Ankush <152695693+ankushT369@users.noreply.github.com>
2025-05-29 19:17:17 +0530

    Add files via upload

---
+++ config.zig
@@ -0,0 +1,7 @@
+pub const ip_address = "127.0.0.1";
+pub const port = 9090;
+pub const MAX_CLIENT = 10;
+// 4KB default page size
+pub const MAX_BUFFER_SIZE = 4096;       
+
+pub const file_name = "/home/ankush/projects/foxMQ/test/save.log";
